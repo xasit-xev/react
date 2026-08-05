c85748a6764c89c7bcff01cd5533e5ae2c7ae7a5
xasit-xev <xasit.xev@gmail.com>
2026-08-05 06:54:11 +0500

    Initial Commit

---
+++ src/Components/About/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>About Kela Kha meri Jan</div>
+    )
+}
+
+export default About
---
+++ src/Components/Home/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>So hi this is my home page!</div>
+    )
+}
+
+export default page
